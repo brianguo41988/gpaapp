64a458419731c495614803cf76cb0a7994a6d793
brianguo41988 <brianguo@live.unc.edu>
2022-03-13 16:26:39 -0400

    Revert "image path require false"
    This reverts commit b4cb088f8b463474f6c5def80233e0732bbbde3e.

--- backend/models/class.js
+++ backend/models/class.js
@@ -5,7 +5,7 @@ const classSchema = mongoose.Schema({
   className: { type: String, required: true },
   classWeight: { type: String, required: true},
   classDes: { type: String, required: true},
-  imagePath: { type: String, required: false},
+  imagePath: { type: String, required: true },
   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true } // ref: reference used for User
 });
 
