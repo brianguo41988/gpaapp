cd35890e697b9d29c4fd836d675546484b5b11c3
brianguo41988 <brianguo@live.unc.edu>
2022-03-16 16:55:01 -0400

    made signup reactive

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Subscription } from "rxjs";
 import { AuthService } from "../auth.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { mimeType } from "../../classes/classes-create/mime-type.validator";
 
 
 @Component({
@@ -12,6 +14,8 @@ import { AuthService } from "../auth.service";
 export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
   private authStatusSub: Subscription;
+  form: FormGroup;
+  imagePreview: string; //convert image to url
   constructor (public authService: AuthService) {}
 
   ngOnInit() {
@@ -20,17 +24,51 @@ export class SignupComponent implements OnInit, OnDestroy {
         this.isLoading = false;
       }
     );
+
+    this.form = new FormGroup({
+      'fname': new FormControl(null, {
+        validators: [Validators.required]}),
+      'lname': new FormControl(null, {
+        validators: [Validators.required]}),
+      'email': new FormControl(null, {
+            validators: [Validators.required]}),
+      'password': new FormControl(null, {
+            validators: [Validators.required]})
+      //       ,
+      // 'image': new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
+     });
   }
 
   ngOnDestroy(){
     this.authStatusSub.unsubscribe();
   }
 
-  onSignup(form: NgForm){
-    if (form.invalid){
+  // onSignup(form: NgForm){
+  //   if (form.invalid){
+  //     return;
+  //   }
+  //   this.isLoading = true;
+  //   this.authService.createUser(form.value.email, form.value.password, form.value.fname, form.value.lname);
+  // }
+
+  onSignup(){
+
+    if (this.form.invalid){
       return;
     }
+
     this.isLoading = true;
-    this.authService.createUser(form.value.email, form.value.password, form.value.fname, form.value.lname);
+    this.authService.createUser(this.form.value.email, this.form.value.password, this.form.value.fname, this.form.value.lname);
   }
+
+  // onImagePicked(event: Event){
+  //   const file = (event.target as HTMLInputElement).files[0];
+  //   this.form.patchValue({image: file});
+  //   this.form.get('image').updateValueAndValidity();
+  //   const reader = new FileReader();
+  //   reader.onload = () => {
+  //     this.imagePreview = reader.result as string;
+  //   };
+  //   reader.readAsDataURL(file);
+  // }
 }
